24e7fda38e71d447858dec9ef14d38396775b386
Dominic Cicilio <dominic.cicilio@tylertech.com>
2021-09-20 09:57:04 -0400

    WIP preview api

---
+++ pages/api/preview.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // ...
+  res.setPreviewData({})
+  res.end('Preview mode enabled')
+  // ...
+}
